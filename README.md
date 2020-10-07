167b2fd0f9a7e15574628036ec3245bf067a5c62
mdt48 <mdt_@gwu.edu>
2020-10-07 11:24:17 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Rishit Bot v0.1
+This is a very bad discord bot to imitate Rishiraj Kanungo. 
---
+++ main.py
@@ -0,0 +1,25 @@
+import discord
+import os
+from dotenv import load_dotenv
+
+bot = discord.Client()
+load_dotenv()
+DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
+@bot.event 
+async def on_ready():
+    guild_count = 0
+
+    for guild in bot.guilds:
+        print(f"- {guild.id} (name: {guild.name})")
+
+        guild_count += 1
+
+    print("rishi-bot is in " + str(guild_count) + " guilds.")    
+
+@bot.event
+async def on_message(message):
+    if message.content == "hello":
+        await message.channel.send("Im geeked")
+
+
+bot.run(DISCORD_TOKEN)
